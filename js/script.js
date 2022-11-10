3b880a42b7082eb47c54b0129ae12ef5279b197a
sercamembert <69466055+sercamembert@users.noreply.github.com>
2022-11-10 18:25:47 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -47,5 +47,26 @@ $('#newsBtn').bind('click', function() {
       validate();
 });
 
+$('#przepisy').bind('click', function(){
+    $('.sidebar').removeClass('translate-sidebar');
+    $('.sidebar').removeClass('translate-sidebar-2');
+    $('.sidebar').addClass('translate-sidebar');
+});
+
+$('#close').bind('click', function(){
+    $('.sidebar').addClass('translate-sidebar-2');
+});
+
+
+let buttons = document.querySelectorAll(".other");
+buttons.forEach(function(item){
+    item.addEventListener('click', function(){
+        $('.sidebar').removeClass('translate-sidebar');
+        $('.sidebar').removeClass('translate-sidebar-2');
+        $('.sidebar').addClass('translate-sidebar');
+    });
+});
+
+
 
 
